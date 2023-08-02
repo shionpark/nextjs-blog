24fa8517c66a476d2e5901cb29c5d957deb22fa3
bkkom <seoy1108@gmail.com>
2023-08-03 01:04:59 +0900

    feat: 사전 렌더링 프로세스 동안 getStaticProps 함수 실행

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import { allPosts } from "contentlayer/generated";
 import Container from "@/components/Container";
+import RecentPosts from "@/components/RecentPosts";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Container>
-      <span className="font-bold">HOME</span>
+      <div className={`my-5 w-full`}>
+        <RecentPosts posts={posts} />
+      </div>
     </Container>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = allPosts.sort(
+    (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+  );
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Home;
