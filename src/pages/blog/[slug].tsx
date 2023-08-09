385aadd3a1bcad5e7a82950b64af1a10c2be0677
bkkom <seoy1108@gmail.com>
2023-08-09 23:59:30 +0900

    feat: 글 post (getStaticPaths: 빌드되어야할 동적 라우트들 명시)

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,36 @@
+import Container from "@/components/Container";
+import { allPosts } from "contentlayer/generated";
+import { InferGetStaticPropsType } from "next";
+import { useMDXComponent } from "next-contentlayer/hooks";
+
+const Post = ({
+  post: any,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const MDXComponent = useMDXComponent(post.body.code);
+  return (
+    <Container>
+      <div className="mt-10 prose">
+        <h1 className="text-sky-700">{post.title}</h1>
+        <MDXComponent />
+      </div>
+    </Container>
+  );
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: allPosts.map((p) => ({ params: { slug: p._raw.flattenedPath } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: any }) => {
+  const post = allPosts.find((p) => p._raw.flattenedPath === params.slug);
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export default Post;
